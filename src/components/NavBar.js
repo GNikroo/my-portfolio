82b79843ac9e53d68269c97f30b6ef0452b6ac51
Gina Nikroo <gina.nikroo@gmail.com>
2023-07-08 22:18:24 +0200

    Change NavLink to Link and correct routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Button, Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
@@ -15,35 +15,34 @@ const NavBar = () => {
   return (
     <Navbar fixed="top" className={`${styles.NavBar} mb-3`}>
       <Container fluid>
-        <Navbar.Brand to="#about" className={styles.Title}>
-          Gina Nikroo
-          <span className={`${styles.Description} d-none d-md-inline`}>
-            <i className="text-white px-3 fa-solid fa-code-commit"></i> Junior
-            Full-Stack Developer
-          </span>
-        </Navbar.Brand>
-        <Nav className="" id="basic-navbar-nav">
-          <NavLink
-            to="#projects"
-            className={`${styles.Links} d-none d-lg-block`}
-          >
+        <Link to="/" className="text-decoration-none">
+          <Navbar.Brand className={styles.Title}>
+            Gina Nikroo
+            <span className={`${styles.Description} d-none d-md-inline`}>
+              <i className="text-white px-3 fa-solid fa-code-commit"></i> Junior
+              Full-Stack Developer
+            </span>
+          </Navbar.Brand>
+        </Link>
+        <Nav id="basic-navbar-nav">
+          <Link to="/about" className={`${styles.Links} d-none d-lg-block`}>
+            About
+          </Link>
+          <Link to="/projects" className={`${styles.Links} d-none d-lg-block`}>
             Projects
-          </NavLink>
-          <NavLink
-            to="#certifications"
+          </Link>
+          <Link
+            to="/certificates"
             className={`${styles.Links} d-none d-lg-block`}
           >
             Certifications
-          </NavLink>
-          <NavLink to="#skills" className={`${styles.Links} d-none d-lg-block`}>
+          </Link>
+          <Link to="/skills" className={`${styles.Links} d-none d-lg-block`}>
             Skills
-          </NavLink>
-          <NavLink
-            to="#contact"
-            className={`${styles.Links} d-none d-lg-block`}
-          >
+          </Link>
+          <Link to="/contact" className={`${styles.Links} d-none d-lg-block`}>
             Contact
-          </NavLink>
+          </Link>
         </Nav>
         <Button
           onClick={handleShow}
@@ -58,34 +57,34 @@ const NavBar = () => {
         >
           <Offcanvas.Header closeButton></Offcanvas.Header>
           <Offcanvas.Body>
-            <NavLink
+            <Link
               onClick={toggleCanvasOff}
               to="#projects"
               className={`${styles.Links} row pb-3`}
             >
               Projects
-            </NavLink>
-            <NavLink
+            </Link>
+            <Link
               onClick={toggleCanvasOff}
               to="#certifications"
               className={`${styles.Links} row pb-3`}
             >
               Certifications
-            </NavLink>
-            <NavLink
+            </Link>
+            <Link
               onClick={toggleCanvasOff}
               to="#skills"
               className={`${styles.Links} row pb-3`}
             >
               Skills
-            </NavLink>
-            <NavLink
+            </Link>
+            <Link
               onClick={toggleCanvasOff}
               to="#contact"
               className={`${styles.Links} row pb-3`}
             >
               Contact
-            </NavLink>
+            </Link>
           </Offcanvas.Body>
         </Offcanvas>
       </Container>
