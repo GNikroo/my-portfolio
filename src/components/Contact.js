e31cb1071c51a61ab181b153ecdc85ec27138892
Gina Nikroo <gina.nikroo@gmail.com>
2023-07-08 15:17:54 +0200

    Minor style changes. Update NavBar for react-routing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Button, Card, CardGroup, Col, Container, Form, Row } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  CardGroup,
+  Col,
+  Container,
+  Form,
+  Row,
+} from "react-bootstrap";
 import appStyles from "../App.module.css";
 import styles from "../styles/Contact.module.css";
 
@@ -28,10 +36,7 @@ const Contact = () => {
   }
 
   return (
-    <Container
-      id="contact"
-      className={`${appStyles.Section} text-center`}
-    >
+    <Container id="contact" className={`${appStyles.Section} text-center`}>
       <Card className={`${styles.Contact} border-0`}>
         <Card.Title className={`${appStyles.Title} pb-2 d-block text-center`}>
           Contact Me
@@ -42,7 +47,7 @@ const Contact = () => {
           <Col lg={7}>
             <Card className={`${styles.FormContainer} border-0 mx-auto px-3`}>
               <Form data-netlify="true" name="contact" onSubmit={handleSubmit}>
-              <input type="hidden" name="form-name" value="contact" />
+                <input type="hidden" name="form-name" value="contact" />
                 <Card.Title className={styles.Subtitle}>Contact Me</Card.Title>
                 <Card.Text className={styles.Description}>
                   As a developer, I am always striving to learn more so that I
@@ -50,56 +55,47 @@ const Contact = () => {
                   continue to develop and add to my skills.
                 </Card.Text>
                 <Row className="px-3">
-                    <Col md={6} sm={1}>
-                        <Form.Group className="relative mb-2">
-                            <Form.Label
-                                htmlFor="name"
-                                className={styles.Label}
-                            >
-                                Name
-                            </Form.Label>
-                            <Form.Control 
-                                type="text"
-                                id="name"
-                                name="name"
-                                autoComplete="given-name"
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                        </Form.Group>
-                        <Form.Group className="relative mb-2">
-                            <Form.Label
-                                htmlFor="email"
-                                className={styles.Label}
-                            >
-                                Email
-                            </Form.Label>
-                            <Form.Control 
-                                type="email"
-                                id="email"
-                                name="email"
-                                autoComplete="email"
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </Form.Group>
-                    </Col>
-                    <Col md={6} sm={1}>
-                        <Form.Group className="relative mb-2">
-                            <Form.Label
-                                htmlFor="message"
-                                className={styles.Label}
-                            >
-                                Message
-                            </Form.Label>
-                            <Form.Control
-                                onChange={(e) => setMessage(e.target.value)}
-                                as="textarea"
-                                aria-label="With textarea"
-                                id="message"
-                                name="message"
-                                rows={5}
-                            />
-                        </Form.Group>
-                    </Col>
+                  <Col md={6} sm={1}>
+                    <Form.Group className="relative mb-2">
+                      <Form.Label htmlFor="name" className={styles.Label}>
+                        Name
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        id="name"
+                        name="name"
+                        autoComplete="given-name"
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                    </Form.Group>
+                    <Form.Group className="relative mb-2">
+                      <Form.Label htmlFor="email" className={styles.Label}>
+                        Email
+                      </Form.Label>
+                      <Form.Control
+                        type="email"
+                        id="email"
+                        name="email"
+                        autoComplete="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col md={6} sm={1}>
+                    <Form.Group className="relative mb-2">
+                      <Form.Label htmlFor="message" className={styles.Label}>
+                        Message
+                      </Form.Label>
+                      <Form.Control
+                        onChange={(e) => setMessage(e.target.value)}
+                        as="textarea"
+                        aria-label="With textarea"
+                        id="message"
+                        name="message"
+                        rows={5}
+                      />
+                    </Form.Group>
+                  </Col>
                 </Row>
                 <Container>
                   <Button
@@ -117,16 +113,14 @@ const Contact = () => {
         <Row className="m-auto p-3 w-100">
           <Card.Body>
             <iframe
-                title="map"  
-                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d65118.06237581816!2d18.066014112133303!3d59.33396474234963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sse!4v1687270474264!5m2!1sen!2sse" 
-                width="100%"
-                height="100%"
-                className="border-0"
-                allowfullscreen="" 
-                loading="lazy" 
-                referrerpolicy="no-referrer-when-downgrade"
-            >
-            </iframe>
+              title="map"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d65118.06237581816!2d18.066014112133303!3d59.33396474234963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sse!4v1687270474264!5m2!1sen!2sse"
+              width="100%"
+              height="100%"
+              className="border-0"
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
           </Card.Body>
         </Row>
       </CardGroup>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Button, Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
@@ -12,88 +13,83 @@ const NavBar = () => {
   };
 
   return (
-    <>
-      <Navbar fixed="top" className={`${styles.NavBar} mb-3`}>
-        <Container fluid>
-          <Navbar.Brand href="#about" className={styles.Title}>
-            Gina Nikroo
-            <span className={`${styles.Description} d-none d-md-inline`}>
-              <i className="text-white px-3 fa-solid fa-code-commit"></i> Junior
-              Full-Stack Developer
-            </span>
-          </Navbar.Brand>
-          <Nav className={styles.Collapse} id="basic-navbar-nav">
-            <Nav.Link
-              href="#projects"
-              className={`${styles.Links} d-none d-lg-block pb-3`}
+    <Navbar fixed="top" className={`${styles.NavBar} mb-3`}>
+      <Container fluid>
+        <Navbar.Brand to="#about" className={styles.Title}>
+          Gina Nikroo
+          <span className={`${styles.Description} d-none d-md-inline`}>
+            <i className="text-white px-3 fa-solid fa-code-commit"></i> Junior
+            Full-Stack Developer
+          </span>
+        </Navbar.Brand>
+        <Nav className="" id="basic-navbar-nav">
+          <NavLink
+            to="#projects"
+            className={`${styles.Links} d-none d-lg-block`}
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="#certifications"
+            className={`${styles.Links} d-none d-lg-block`}
+          >
+            Certifications
+          </NavLink>
+          <NavLink to="#skills" className={`${styles.Links} d-none d-lg-block`}>
+            Skills
+          </NavLink>
+          <NavLink
+            to="#contact"
+            className={`${styles.Links} d-none d-lg-block`}
+          >
+            Contact
+          </NavLink>
+        </Nav>
+        <Button
+          onClick={handleShow}
+          className={`${styles.Button} d-lg-none border-0`}
+        >
+          <i className="fa-solid fa-bars"></i>
+        </Button>
+        <Offcanvas
+          className={styles.Offcanvas}
+          show={show}
+          onHide={handleClose}
+        >
+          <Offcanvas.Header closeButton></Offcanvas.Header>
+          <Offcanvas.Body>
+            <NavLink
+              onClick={toggleCanvasOff}
+              to="#projects"
+              className={`${styles.Links} pb-3`}
             >
               Projects
-            </Nav.Link>
-            <Nav.Link
-              href="#certifications"
-              className={`${styles.Links} d-none d-lg-block pb-3`}
+            </NavLink>
+            <NavLink
+              onClick={toggleCanvasOff}
+              to="#certifications"
+              className={`${styles.Links} pb-3`}
             >
               Certifications
-            </Nav.Link>
-            <Nav.Link
-              href="#skills"
-              className={`${styles.Links} d-none d-lg-block pb-3`}
+            </NavLink>
+            <NavLink
+              onClick={toggleCanvasOff}
+              to="#skills"
+              className={`${styles.Links} pb-3`}
             >
               Skills
-            </Nav.Link>
-            <Nav.Link
-              href="#contact"
-              className={`${styles.Links} d-none d-lg-block pb-3`}
+            </NavLink>
+            <NavLink
+              onClick={toggleCanvasOff}
+              to="#contact"
+              className={`${styles.Links} pb-3`}
             >
               Contact
-            </Nav.Link>
-          </Nav>
-          <Button
-            onClick={handleShow}
-            className={`${styles.Button} d-lg-none border-0`}
-          >
-            <i className="fa-solid fa-bars"></i>
-          </Button>
-          <Offcanvas
-            className={styles.Offcanvas}
-            show={show}
-            onHide={handleClose}
-          >
-            <Offcanvas.Header closeButton></Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav.Link
-                onClick={toggleCanvasOff}
-                href="#projects"
-                className={`${styles.Links} pb-3`}
-              >
-                Projects
-              </Nav.Link>
-              <Nav.Link
-                onClick={toggleCanvasOff}
-                href="#certifications"
-                className={`${styles.Links} pb-3`}
-              >
-                Certifications
-              </Nav.Link>
-              <Nav.Link
-                onClick={toggleCanvasOff}
-                href="#skills"
-                className={`${styles.Links} pb-3`}
-              >
-                Skills
-              </Nav.Link>
-              <Nav.Link
-                onClick={toggleCanvasOff}
-                href="#contact"
-                className={`${styles.Links} pb-3`}
-              >
-                Contact
-              </Nav.Link>
-            </Offcanvas.Body>
-          </Offcanvas>
-        </Container>
-      </Navbar>
-    </>
+            </NavLink>
+          </Offcanvas.Body>
+        </Offcanvas>
+      </Container>
+    </Navbar>
   );
 };
 
