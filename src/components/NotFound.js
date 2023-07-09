28d9a865e50a51019f6dbdba7731762327a08442
Gina Nikroo <gina.nikroo@gmail.com>
2023-07-09 21:05:23 +0200

    Update NotFound page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import styles from "../styles/NotFound.module.css";
 import appStyles from "../App.module.css";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
@@ -18,6 +19,10 @@ const NotFound = () => {
           </Row>
         </h1>
         <h2 className={styles.Subtitle}>this page does not exist!</h2>
+        <Link to="/" className={`${styles.Link} text-decoration-none`}>
+          go home <span className={styles.Line}>&ndash;</span>
+          <span className={styles.Arrow}>&rsaquo;</span>
+        </Link>
       </Card>
     </Container>
   );
